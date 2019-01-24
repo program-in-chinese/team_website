6ea0691ffb3739f51e88371ce53eedfd102afba4
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-01-23 22:34:17 -0800

    改进绘制

--- docs/圈3/p5js中文.js
+++ docs/圈3/p5js中文.js
@@ -29,6 +29,10 @@ function draw() {
 function 绘制() {};
 
 // 二维基本元素
+function 线段(起点x, 起点y, 终点x, 终点y) {
+	line(起点x, 起点y, 终点x, 终点y);
+}
+
 function 长方形(x位置, y位置, 宽, 高) {
 	rect(x位置, y位置, 宽, 高);
 }
@@ -184,4 +188,8 @@ function 新画布(宽, 高) {
 // 模式为P2D或者WEBGL
 function 新画布(宽, 高, 模式) {
 	return createCanvas(宽, 高, 模式);
+}
+
+function 清理() {
+	clear();
 }
