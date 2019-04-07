77770448cb43bd8e5692177f62e274e62b49ec04
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-04-06 19:30:51 -0700

    尝试刷新插件, 添加全文搜索

--- docs/_plugins/simple_search_filter_cn.rb
+++ docs/_plugins/simple_search_filter_cn.rb
@@ -3,7 +3,7 @@ module Jekyll
     def remove_chars_cn(input)
       input.gsub! '\\','&#92;'
       input.gsub! /\t/, '    '
-	  input.gsub! '@',''
+      input.gsub! '@',''
 	  input.gsub! '$',''
 	  input.gsub! '%',''
 	  input.gsub! '&',''
